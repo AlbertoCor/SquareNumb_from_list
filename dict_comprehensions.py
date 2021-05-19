4469e319f7228baa904273d777635daa3c804156
Alberto Coronado <ac.insite15@live.com>
2021-05-19 18:03:04 -0500

    Update list comprehensions for method to resolve

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -1,9 +1,11 @@
 def run():
-    my_dict = {}
+    # my_dict = {}
 
-    for i in range(1, 100):
-        if i % 3 !=0:
-            my_dict[i] = i**3
+    # for i in range(1, 100):
+    #     if i % 3 !=0:
+    #         my_dict[i] = i**3
+
+    my_dict = {i: i**3 for i in range(1,100) if i % 3 != 0}
 
     print(my_dict)
 
