a10afda679ef93545a0e2196946fc085a7423f7b
AlbertoCor <ac.insite15@live.com>
2021-05-20 23:35:34 -0500

    data_filter_challenge.py

---
+++ data_filter_challenge.py
@@ -0,0 +1,101 @@
+   
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    all_python_devs = list(filter(lambda worker: worker["language"] == "python", DATA))
+    all_python_devs = list(map(lambda worker: worker['name'], all_python_devs))
+
+    for worker in all_python_devs:
+        print(worker)
+
+    all_platzi_workers = list(filter(lambda worker: worker["organization"] == "Platzi", DATA))
+    all_platzi_workers = list(map(lambda worker: worker['name'], all_platzi_workers))
+
+    print(" \n")
+
+    for worker in all_platzi_workers:
+        print(worker)
+        
+
+    old_people = [worker for worker in DATA if worker["age"] > 70]
+    print(f"\n {old_people} \n")
+
+    adult = [worker for worker in DATA if worker["age"] < 70]
+    
+    for worker in adult:
+        print(worker)
+
+
+if __name__ == "__main__":
+    run()
